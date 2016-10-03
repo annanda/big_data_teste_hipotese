dff3d127b46fae98148e3262a9cbe2d0f63681fb
Annanda Sousa <annanda.sousa@gmail.com>
2016-10-03 18:18:00 -0300

    Fazendo a primeira parte da questão 3. Usando correlação de spearman.

--- movie_metadata.py
+++ movie_metadata.py
@@ -1,11 +1,27 @@
 import csv
+import numpy as np
+from scipy import stats
 
 with open('data/movie_metadata.csv', 'r') as arquivo:
     data = csv.reader(arquivo, delimiter=",")
 
-    dados = []
+    var_1 = []
+    var_2 = []
 
     for i, line in enumerate(data):
         if i != 0:
-            dados.append([line[15], line[25]])
+            if line[15] != '':
+                var_1.append(line[15])
+            else:
+                var_1.append('0')
 
+            var_2.append(line[25])
+
+
+var_1 = np.array(var_1, dtype='f')
+var_2 = np.array(var_2, dtype='f')
+
+correferencia, p_valor = stats.spearmanr(var_1, var_2)
+
+print(correferencia)
+print(p_valor)
