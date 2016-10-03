30d271797c3f73a2372b267c07327c5b053974f9
Annanda Sousa <annanda.sousa@gmail.com>
2016-10-03 20:58:23 -0300

    Tirando código não usado.

--- movie_metadata.py
+++ movie_metadata.py
@@ -36,18 +36,3 @@ print(coeficiente)
 print(p_valor)
 print(tamanho_amostra)
 print(significancia_spearman(coeficiente, tamanho_amostra))
-
-
-
-h = sorted([186, 176, 158, 180, 186, 168, 168, 164, 178, 170, 189, 195, 172,
-     187, 180, 186, 185, 168, 179, 178, 183, 179, 170, 175, 186, 159,
-     161, 178, 175, 185, 175, 162, 173, 172, 177, 175, 172, 177, 180])  #sorted
-
-fit = stats.norm.pdf(h, np.mean(h), np.std(h))  #this is a fitting indeed
-
-pl.plot(var_1,'o', color="red")
-pl.plot(var_2, 'o', color="blue")
-
-# pl.hist(h,normed=True)      #use this to draw histogram of your data
-
-pl.show()
